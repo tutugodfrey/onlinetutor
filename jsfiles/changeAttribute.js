cb980196aca937e77cf0243c420430890db48463
tutu godfrey <godfrey_tutu@yahoo.com>
2018-01-08 03:37:24 +0100

    define function to attribute of html elements

---
+++ jsfiles/changeAttribute.js
@@ -0,0 +1,15 @@
+export const changeAttribute  = function(eleObject, attrToChange, newAttr, removeAttr) {
+  const eleAttr = eleObject.getAttribute(attrToChange);
+  if (eleAttr === null || eleAttr !== newAttr) 	{
+    //set the attribute and the image become large
+    eleObject.setAttribute(attrToChange, newAttr);
+  } else if (eleAttr === newAttr && removeAttr === "yes") {
+    eleObject.removeAttribute(attrToChange);
+  }  else if (eleAttr === newAttr && removeAttr === "no") {
+    //no action taken
+  } else if(eleAttr === newAttr && (removeAttr !== "yes" || removeAttr !== "no" || removeAttr !== "undefined")){
+    eleObject.setAttribute(attrToChange, removeAttr);
+  } 
+}		//end changeClass
+
+export default changeAttribute
