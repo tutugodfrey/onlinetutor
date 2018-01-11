0e8df37695159cc218e2a93cb4550856e89ca4f5
tutu godfrey <godfrey_tutu@yahoo.com>
2018-01-11 01:14:50 +0100

    modify path to bundle on index.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ echo <<<block
       </div>
     </div>
     <script src = 'jsfiles/bootstrap4_js/bootstrap.min.js' ></script>
-    <script src = '/onlinetutor/dist/bundle.js'></script>
+    <script src = 'dist/bundle.js'></script>
     <script type = "text/javascript" src = "./jsfiles/quotes.js"></script>
   </body>
 </html>
