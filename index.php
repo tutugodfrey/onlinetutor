7583d88a28f08c5236ba521e06ed0f84e3b0f889
tutu godfrey <godfrey_tutu@yahoo.com>
2018-03-17 13:10:42 +0100

    include favicon and  logo

--- index.php
+++ index.php
@@ -8,17 +8,18 @@ echo <<<block
     </title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel = "shortcut icon" href = "/images/onlinetutor.ico" type = "image/x-icon" />
     <link type = "text/css" rel = "stylesheet" href = "mylecapp_css/bootstrap4_css/bootstrap.min.css" />
     <link type = "text/css" rel = "stylesheet" href = "mylecapp_css/style1.css" />
   </head>
   <body>
-    <div>
+    <div class = "container">
       <div id = "header-div">
       </div>
       <div><p id = "ajax_neutral">Processing ...</p></div>
-      <div id = "content-area"> 
+      <div id = "content-area" class = ""> 
       </div>
-      <div id = "footer-div" class = "navbar fixed-bottom">
+      <div id = "footer-div" class = "navbar">
       </div>
     </div>
     <script src = "jsfiles/tether-1.3.3/dist/js/tether.min.js" ></script>
